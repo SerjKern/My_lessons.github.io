4d0a874fe60876fb981e3abcc6a8adbe2c6232d5
Sergey <kernserj@gmail.com>
2024-03-16 21:41:52 +0300

    Тренировка в jquery, переделал sass в scss

--- Lesson_23/PROJECT/src/js/modal.js
+++ Lesson_23/PROJECT/src/js/modal.js
@@ -9,4 +9,19 @@ $(document).ready(function () {
   close.on("click", function () {
     modal.removeClass("modal_active");
   });
+
+  let logo = $(".navbar__logo");
+  console.log("logo--", logo);
+  let phone = $(".navbar__phone");
+  let title = $(".hero-text__title");
+  title.on("click", function(){
+   phone.show("slow");
+  })
+  
+  logo.on("click", function (e) {
+    console.log("phone--", phone);
+    phone.hide("slow");
+    // console.log("это клик--", e.currentTarget);
+  });
+
 });
